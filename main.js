c683af0ecd2ca45c7ad786dd0435afe9f8e6a199
muyook <yookm106@newschool.edu>
2016-11-14 19:09:22 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $(".hide-btn").click(function(){
+
+   $("li").hide("slow");
+
+  });
+
+   $(".show-btn").click(function(){
+
+    $("li").show("slow");
+
+  });
+
+})
